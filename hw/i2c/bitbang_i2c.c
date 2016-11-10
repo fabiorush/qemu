ec7dc6ab1c9ed2ebe8839bc751fa00b140195344
Fabio Urquiza <fabio.urquiza@hpe.com>
2016-11-10 16:15:41 -0300

    Implements NACK on I2C transfers when the busy flag is set.
    Signed-off-by: Fabio Urquiza <fabio.urquiza@hpe.com>

--- hw/i2c/bitbang_i2c.c
+++ hw/i2c/bitbang_i2c.c
@@ -82,7 +82,7 @@ static int bitbang_i2c_nop(bitbang_i2c_interface *i2c)
 /* Returns data line level.  */
 int bitbang_i2c_set(bitbang_i2c_interface *i2c, int line, int level)
 {
-    int data;
+    int data, ret = 0;
 
     if (level != 0 && level != 1) {
         abort();
@@ -133,19 +133,23 @@ int bitbang_i2c_set(bitbang_i2c_interface *i2c, int line, int level)
         if (i2c->current_addr < 0) {
             i2c->current_addr = i2c->buffer;
             DPRINTF("Address 0x%02x\n", i2c->current_addr);
-            i2c_start_transfer(i2c->bus, i2c->current_addr >> 1,
+            ret = i2c_start_transfer(i2c->bus, i2c->current_addr >> 1,
                                i2c->current_addr & 1);
         } else {
             DPRINTF("Sent 0x%02x\n", i2c->buffer);
             i2c_send(i2c->bus, i2c->buffer);
         }
-        if (i2c->current_addr & 1) {
-            i2c->state = RECEIVING_BIT7;
+        if (ret < 0) {
+            bitbang_i2c_enter_stop(i2c);
+            return bitbang_i2c_ret(i2c, 1);
         } else {
-            i2c->state = SENDING_BIT7;
+            if (i2c->current_addr & 1) {
+                i2c->state = RECEIVING_BIT7;
+            } else {
+                i2c->state = SENDING_BIT7;
+            }
+            return bitbang_i2c_ret(i2c, 0);
         }
-        return bitbang_i2c_ret(i2c, 0);
-
     case RECEIVING_BIT7:
         i2c->buffer = i2c_recv(i2c->bus);
         DPRINTF("RX byte 0x%02x\n", i2c->buffer);
